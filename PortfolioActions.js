afeffa1488f73181598bad927501c5c174a28f99
SeanC55417 <126941875+SeanC55417@users.noreply.github.com>
2024-09-09 14:24:00 -0400

    created the first iteration of the homepage still looking to change the nav and footer

---
+++ PortfolioActions.js
@@ -0,0 +1,15 @@
+function RedirectHome() {
+    window.location.href = "PortfolioHome.html";
+}
+
+function RedirectWorkHistory() {
+    window.location.href = "WorkHistory.html";
+}
+
+function RedirectProjects() {
+    window.location.href = "Projects.html";
+}
+
+function RedirectContactInfo() {
+    window.location.href = "ContactInfo.html";
+}
